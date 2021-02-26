00b290291636430d073bdc68183008b6d13e3e1d
William Blair <60581672+williamblair333@users.noreply.github.com>
2021-02-26 14:24:04 -0500

    Add files via upload
    Fixed dumb indent error and wrapped text for easier reading

--- img-svg-converter.py
+++ img-svg-converter.py
@@ -13,8 +13,8 @@ import subprocess
 
 sg.ChangeLookAndFeel('Reddit')
 
-layout =  [sg.Text('Image Source ', size=(15, 1)), sg.InputText(), sg.FileBrowse()],
-          [sg.Submit(), sg.Cancel()]]
+layout =  [sg.Text('Image Source ', size=(15, 1)), sg.InputText(), \
+           sg.FileBrowse()], [sg.Submit(), sg.Cancel()]
 
 window = sg.Window('Image to SVG Converter - Insert IT Dept Name Here', layout)
 
@@ -28,7 +28,8 @@ src_filename = values[0]
 getpath = pathlib.Path(src_filename)
 src_pathname = getpath.parent
 
-dest_filename = str(src_pathname) + "\\" + (os.path.splitext(os.path.basename(src_filename))[0]) + ext_svg
+dest_filename = str(src_pathname) + "\\" + \
+                (os.path.splitext(os.path.basename(src_filename))[0]) + ext_svg
 
 src_filename = src_filename.replace("/", "\\")
 
@@ -37,7 +38,8 @@ print(dest_filename)
 
 # You will need to install ImageMagick in order to do the actual conversion. 
 # Change path as appropriate for location and OS
-conversion = "C:\\util\\ImageMagick-7.0.10-30-portable-Q16-HDRI-x64\\magick.exe" + " " + src_filename + " " + dest_filename
+conversion = "C:\\util\\ImageMagick-7.0.10-30-portable-Q16-HDRI-x64\\magick.exe" + \
+             " " + src_filename + " " + dest_filename
 
 print(conversion)
 subprocess.call(conversion)
